056ac84871a8da8cb1d64ec40e7c105e55474426
Nico <nicolasmega01.08.94@gmail.com>
2025-10-29 01:47:31 -0300

    creando clase Investigador

---
+++ tpi/Investigador.java
@@ -0,0 +1,14 @@
+public class Investigador {
+
+    private String nombre;
+    private int edad;
+    private int cantidadExperimentos;
+
+    public Investigador(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cantidadExperimentos = 0;
+    }
+
+
+}
